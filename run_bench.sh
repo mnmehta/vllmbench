58431435793a400146154e940035925ed45f238f
mnmehta <30246802+mnmehta@users.noreply.github.com>
2025-10-09 19:02:10 -0400

    a few changes to support precise kvcache mode

--- run_bench.sh
+++ run_bench.sh
@@ -5,6 +5,11 @@ python install_llmd.py --config-name llama70b_precise
 MLFLOW_TRACKING_URI=http://169.63.180.173:5000 python bench.py --config-name llama70b_precise run.experiment_name=approximate_vs_precise
 python install_llmd.py --config-name llama70b
 MLFLOW_TRACKING_URI=http://169.63.180.173:5000 python bench.py --config-name llama70b run.experiment_name=approximate_vs_precise
+
+python install_llmd.py --config-name default install.well_lit_path=guides/precise-prefix-cache-aware
+MLFLOW_TRACKING_URI=http://169.63.180.173:5000 python bench.py --config-name default install.well_lit_path=guides/precise-prefix-cache-aware run.experiment_name=approximate_vs_precise
+python install_llmd.py --config-name default install.well_lit_path=guides/inference-scheduling
+MLFLOW_TRACKING_URI=http://169.63.180.173:5000 python bench.py --config-name default install.well_lit_path=guides/inference-scheduling run.experiment_name=approximate_vs_precise
 exit
 for wlp in precise-prefix-cache-aware inference-scheduling; do
   for config in default llama70b; do
